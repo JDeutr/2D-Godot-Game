8d5a3d9303aaa7a08296cb42b28d9e28ad6876a0
JDeutr <deutrom10@gmail.com>
2026-09-24 22:19:26 +0200

    added player animations and mouse following

--- Player/player.gd
+++ Player/player.gd
@@ -10,11 +10,5 @@ var speed: float
 
 func _ready() -> void:
 	speed = base_speed
-	_assign_player_ref(move_state_machine)
-	_assign_player_ref(combat_state_machine)
 	move_state_machine.start()
 	combat_state_machine.start()
-
-func _assign_player_ref(sm: StateMachine) -> void:
-	for state in sm.states.values():
-		state.player = self
